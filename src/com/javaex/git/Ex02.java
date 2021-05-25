ec81414e180263f2a71cca8aa14814863cde8529
taehee <taehee@LAPTOP-M74B47LU>
2021-05-25 11:02:03 +0900

    5/2 코딩

--- src/com/javaex/git/Ex02.java
+++ src/com/javaex/git/Ex02.java
@@ -1,10 +1,9 @@
 package com.javaex.git;
 
-public class Ex01 {
+public class Ex02 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("5/1 코드");
 		System.out.println("5/2 코드");
 
 	}
